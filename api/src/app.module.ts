1af9cd96ed799a4b2a0127eceff17c813d956d55
Danai Thitipornchai <danai.thit@gmail.com>
2024-04-23 14:26:53 +0700

    Update app.module.ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports:[AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [ {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    }],
 })
 export class AppModule {}
